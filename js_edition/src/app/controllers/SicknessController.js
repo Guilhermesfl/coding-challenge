868e6f0d253339cddf5d35211888f4e824921c00
Guilhermesfl <guilherme@loopce.com>
2019-11-03 16:01:54 -0300

    :sparkles: Added SicknessController and a route to get sick members
    Route /absences/sickness returns employees absences due to sickness

---
+++ js_edition/src/app/controllers/SicknessController.js
@@ -0,0 +1,31 @@
+import { absences, members } from '../../api';
+
+class SicknessController {
+  async index(req, res) {
+    try {
+      let _absences = await absences();
+      const _members = await members();
+      let response = [];
+
+      _absences = _absences.filter((e) => e.type === 'sickness');
+
+      _absences.forEach((absence) => {
+        const abs = absence;
+        for (let i = 0; i < _members.length; i++) {
+          if (abs.userId === _members[i].userId) {
+            response.push(`${_members[i].name} is sick`);
+            break;
+          }
+        }
+      });
+
+      response = [...new Set(response)];
+
+      return res.json(response);
+    } catch (e) {
+      return res.json({ error: 'Error retrieving members that are sick' });
+    }
+  }
+}
+
+export default new SicknessController();
--- js_edition/src/routes.js
+++ js_edition/src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import AbsenceController from './app/controllers/AbsenceController';
 import VacationController from './app/controllers/VacationController';
+import SicknessController from './app/controllers/SicknessController';
 
 const routes = new Router();
 
 routes.get('/absences', AbsenceController.index);
 routes.get('/absences/vacations', VacationController.index);
+routes.get('/absences/sickness', SicknessController.index);
 
 export default routes;
