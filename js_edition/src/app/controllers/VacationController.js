13279f7ed1d70a8a7d9f090736ca4a4bdbe40a02
Guilhermesfl <guilherme@loopce.com>
2019-11-03 15:42:14 -0300

    :sparkles: Added VacationController and a route to get members on vacation
    Route /absences/vacations returns employees absences due to vacation

---
+++ js_edition/src/app/controllers/VacationController.js
@@ -0,0 +1,30 @@
+import { absences, members } from '../../api';
+
+class VacationController {
+  async index(req, res) {
+    try {
+      const _absences = await absences();
+      const _members = await members();
+      let response = [];
+
+      _absences.map((e) => e.type === 'vacation');
+
+      _absences.forEach((absence) => {
+        const abs = absence;
+        for (let i = 0; i < _members.length; i++) {
+          if (abs.userId === _members[i].userId) {
+            response.push(`${_members[i].name} is on vacation`);
+            break;
+          }
+        }
+      });
+      response = [...new Set(response)];
+
+      return res.json(response);
+    } catch (e) {
+      return res.json({ error: 'Error retrieving members on vacation' });
+    }
+  }
+}
+
+export default new VacationController();
--- js_edition/src/routes.js
+++ js_edition/src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import AbsenceController from './app/controllers/AbsenceController';
+import VacationController from './app/controllers/VacationController';
 
 const routes = new Router();
 
 routes.get('/absences', AbsenceController.index);
+routes.get('/absences/vacations', VacationController.index);
 
 export default routes;
