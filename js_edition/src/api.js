3fbc3c198ca9c347987c0832f99f4d305f329cb2
Guilhermesfl <guilherme@loopce.com>
2019-11-03 14:22:10 -0300

    :rotating_light: Fixed shadowed variable lint warning

--- js_edition/src/api.js
+++ js_edition/src/api.js
@@ -4,7 +4,7 @@ import path from 'path';
 const ABSENCES_PATH = path.join(__dirname, 'json_files', 'absences.json');
 const MEMBERS_PATH = path.join(__dirname, 'json_files', 'members.json');
 
-const readJsonFile = (path) => new Promise((resolve) => fs.readFile(path, 'utf8', (_, data) => resolve(data)))
+const readJsonFile = (filePath) => new Promise((resolve) => fs.readFile(filePath, 'utf8', (_, data) => resolve(data)))
   .then((data) => JSON.parse(data))
   .then((data) => data.payload);
 
