8f95780d4608d0c06c22dbcf73fbe776214c02b5
Guilhermesfl <guilherme@loopce.com>
2019-11-03 21:26:16 -0300

    :sparkles: Absences can now be filtered by user id
    If user id is a query param of the request, the absences will be filtered acconding to it

--- js_edition/src/app/controllers/AbsenceController.js
+++ js_edition/src/app/controllers/AbsenceController.js
@@ -3,22 +3,38 @@ import { absences, members } from '../../api';
 class AbsenceController {
   async index(req, res) {
     try {
-      const _absences = await absences();
-      const _members = await members();
-      const response = [];
+      let _absences = await absences();
+      let _members = await members();
 
-      _absences.forEach((absence) => {
+      const { userId } = req.query;
+
+      if (userId) {
+        _absences = _absences.filter((e) => String(e.userId) === userId);
+        _members = _members.filter((e) => String(e.userId) === userId);
+
+        if (_absences.length > 0 && _members.length > 0) {
+          _absences.map((el) => {
+            const abs = el;
+            abs.name = _members[0].name;
+            return abs;
+          });
+          return res.json(_absences);
+        }
+
+        return res.json({ error: 'Member not found with provided user id' });
+      }
+
+      _absences.map((absence) => {
         const abs = absence;
         for (let i = 0; i < _members.length; i++) {
           if (abs.userId === _members[i].userId) {
             abs.userName = _members[i].name;
-            response.push(abs);
-            break;
+            return abs;
           }
         }
       });
 
-      return res.json(_absences);
+      return res.json({ total: _absences.length, absences: _absences });
     } catch (err) {
       return res.json({ error: 'Error retrieving absences' });
     }
